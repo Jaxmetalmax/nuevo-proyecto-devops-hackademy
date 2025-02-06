3cd5de02f28f6ab15c762dbbbc0d87b9a34affd9
Max J. Rodriguez B. <max@intellion.ai>
2025-02-05 19:29:31 -0700

    Añadi funcion de helpers.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from helpers import print_hello
+
 def main():
     print("hola desde mi casa")
     print("un cambio completo")
     print("Un cambio hecho en el trabajo y posterior")
 
 main
+
+print_hello("Max")
